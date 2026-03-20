6601f20df1059e29f98ea595ca12e35545c321f1
Kolezhniuk <dmytro.k@privado.id>
2026-03-20 16:24:06 +0700

    Refactor buildwasm_mnt6753.js to use baseTables from pedersenParamsCode and improve code readability

--- tools/buildwasm_mnt6753.js
+++ tools/buildwasm_mnt6753.js
@@ -1,11 +1,12 @@
 import { ModuleBuilder } from "wasmbuilder";
 import buildMNT6753 from "../src/mnt6753/build_mnt6753.js";
 import buildPedersenHash from "../src/build_pedersenhash.js";
-import baseTables from "../build/pedersenparams_mnt6753.js";
+import { code as pedersenParamsCode } from "../build/pedersenparams_mnt6753.js";
 import { writeFileSync } from "fs";
 import { join, dirname } from "path";
 import { fileURLToPath } from "url";
 const __dirname = dirname(fileURLToPath(import.meta.url));
+
 
 function buildWasm() {
 
@@ -13,12 +14,14 @@ function buildWasm() {
     moduleBuilder.setMemory(1000);
 
     buildMNT6753(moduleBuilder);
+    const baseTables = Uint8Array.from(atob(pedersenParamsCode), c => c.charCodeAt(0));
+
     buildPedersenHash(moduleBuilder, "g1m", "g1m", "f1m", 188, baseTables);
 
     const code = moduleBuilder.build();
 
     writeFileSync(
-        join( __dirname, "..", "build", "mnt6753_wasm.js"),
+        join(__dirname, "..", "build", "mnt6753_wasm.js"),
         `export const code = "${Buffer.from(code).toString("base64")}";
 export const pq = ${moduleBuilder.modules.mnt6753.pq};
 export const pr = ${moduleBuilder.modules.mnt6753.pq};
